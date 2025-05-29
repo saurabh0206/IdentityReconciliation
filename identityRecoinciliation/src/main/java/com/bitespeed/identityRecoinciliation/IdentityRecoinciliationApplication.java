d612abbdbb49ec72e9c0801e375816b531a1cf70
Saurabh <saurabhkumar0206@gmail.com>
2025-05-29 08:31:53 +0530

    Intial setup with package ENTITY

---
+++ identityRecoinciliation/src/main/java/com/bitespeed/identityRecoinciliation/IdentityRecoinciliationApplication.java
@@ -0,0 +1,13 @@
+package com.bitespeed.identityRecoinciliation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityRecoinciliationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityRecoinciliationApplication.class, args);
+	}
+
+}
